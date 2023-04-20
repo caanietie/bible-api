db2b0190663ef830b1780bf04a0e9e68de9a9fed
Anietie <aakjhonclems@gmail.com>
2023-04-20 16:47:47 +0100

    Added wordlist and wordlistverses to schema file

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2038_12_25_111839) do
+ActiveRecord::Schema[7.0].define(version: 2038_12_25_111841) do
   create_table "books", force: :cascade do |t|
     t.string "name", null: false
     t.datetime "created_at", null: false
@@ -40,7 +40,25 @@ ActiveRecord::Schema[7.0].define(version: 2038_12_25_111839) do
     t.index ["verse"], name: "index_verses_on_verse"
   end
 
+  create_table "wordlists", force: :cascade do |t|
+    t.string "word"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["word"], name: "index_wordlists_on_word"
+  end
+
+  create_table "wordlistverses", force: :cascade do |t|
+    t.integer "verse_id", null: false
+    t.integer "wordlist_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["verse_id"], name: "index_wordlistverses_on_verse_id"
+    t.index ["wordlist_id"], name: "index_wordlistverses_on_wordlist_id"
+  end
+
   add_foreign_key "chapters", "books"
   add_foreign_key "verses", "books"
   add_foreign_key "verses", "chapters"
+  add_foreign_key "wordlistverses", "verses"
+  add_foreign_key "wordlistverses", "wordlists"
 end
