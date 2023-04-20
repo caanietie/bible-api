fb3e1619daf6385ff74cef4aa2efdba6080337ce
Anietie <aakjhonclems@gmail.com>
2023-04-20 16:01:41 +0100

    Converted `SearchController.rb` file to a module

--- app/controllers/api/search_controller.rb
+++ app/controllers/api/search_controller.rb
@@ -1,21 +1,51 @@
-class API::SearchController < API::ApplicationController
-  def show
-    # TODO: Implement this method
-    # Traverse the database and return the link to bible passage
-    render json: {
-      search: {option: params[:id]},
-      data: [
-        {
-          book_id: 1, book_name: "Genesis",
-          chapter_id: 120, chapter: 1, 
-          verse_id: 1200, verse: 5
-        },
-        {
-          book_id: 66, book_name: "Revelation",
-          chapter_id: 1220, chapter: 10,
-          verse_id: 12229, verse: 10
+module API
+  class SearchController < ApplicationController
+    def show
+      render json: {
+        search: {
+          value: params[:id],
+          data: search_word(params[:id].split.join(' ').downcase)
         }
-      ]
-    }
+      }
+    end
+
+    private
+
+    def search_word params_id
+      if params_id.split.length.eql? 1
+        Wordlist.find_by(word: params_id)&.wordlistverses
+            &.map(&:verse)&.each_with_object([]) do |verse, obj|
+          obj.push result_hash(params_id, verse)
+        end || []
+      else
+        search_phrase params_id
+      end
+    end
+
+    def search_phrase params_id
+      Verse.all.filter{|v| v.info_text.downcase.include?(params_id)}
+          .each_with_object([]) do |verse, obj|
+        obj.push result_hash(params_id, verse)
+      end
+    end
+
+    def result_hash params_id, verse
+      chapter = verse.chapter
+      book = verse.book
+      {
+        book_id: book.id, book_name: book.name,
+        chapter_id: chapter.id, chapter: chapter.chapter,
+        verse_id: verse.id, verse: verse.verse,
+        info_text: verse_info(params_id, verse.info_text)
+      }
+    end
+
+    def verse_info params_id, info_text
+      arry = info_text.split
+      return info_text if arry.length <= 10
+      frst = arry[...arry.length / 2].join ' '
+      scnd = arry[arry.length / 2 ..].join ' '
+      frst.match?(/#{params_id}/i) ? "#{frst} ..." : "... #{scnd}"
+    end
   end
 end
