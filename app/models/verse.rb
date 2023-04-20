444a8c4b12d7c7e45d49ee6f81345fd435388ff8
Anietie <aakjhonclems@gmail.com>
2023-04-20 16:18:36 +0100

    - Added `wordlist` and `wordlistverses` fields to book model. This helps to search for books. - Removed uniqueness constraint on `info_html` field. Somehow this constraint is violated in verses data.

--- app/models/verse.rb
+++ app/models/verse.rb
@@ -1,10 +1,15 @@
 class Verse < ApplicationRecord
   LONGEST = 176 # Psalms 119
   SHORTEST = 10 # Jesus wept
+
   belongs_to :book
   belongs_to :chapter
+
+  has_many :wordlistverses, dependent: :destroy
+  has_many :wordlists, through: :wordlistverses, dependent: :destroy
+
   validates :verse, 
     numericality: { only_integer: true, in: 1 .. LONGEST }
-  validates :info_html, uniqueness: true, length: { minimum: SHORTEST }
+  validates :info_html, length: { minimum: SHORTEST }
   validates :info_text, length: { minimum: SHORTEST }
 end
