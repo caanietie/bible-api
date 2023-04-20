e609cf9133d89f3549a6e03364c9a917d39f9e89
Anietie <aakjhonclems@gmail.com>
2023-04-20 16:14:05 +0100

    Added `wordlist` and `wordlistverses` fields to book model. This helps to search for books.

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,7 +1,11 @@
 class Book < ApplicationRecord
   SHORTEST = 3 # Job
   LONGEST = 15 # 1 Thessalonians
+
   has_many :chapters, dependent: :destroy
   has_many :verses, through: :chapters, dependent: :destroy
+  has_many :wordlistverses, through: :verses, dependent: :destroy
+  has_many :wordlist, through: :wordlistverses, dependent: :destroy
+
   validates :name, uniqueness: true, length: { in: SHORTEST .. LONGEST }
 end
