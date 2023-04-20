814e7a44c57e1f8b67a04a7d21a5ffc23c30a402
Anietie <aakjhonclems@gmail.com>
2023-04-20 16:16:40 +0100

    Added `wordlist` and `wordlistverses` fields/association to chapter model. This helps to search for chapters.

--- app/models/chapter.rb
+++ app/models/chapter.rb
@@ -1,7 +1,12 @@
 class Chapter < ApplicationRecord
   LONGEST = 150 # Psalms
+
   belongs_to :book
+
   has_many :verses, dependent: :destroy
+  has_many :wordlistverses, through: :verses, dependent: :destroy
+  has_many :wordlists, through: :wordlistverses, dependent: :destroy
+
   validates :chapter,
     numericality: { only_integer: true, in: 1 .. LONGEST }
   validates :info_html, presence: true, uniqueness: true
